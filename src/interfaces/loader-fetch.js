6af62b3763b93c7cbb891a318bf23c9313230530
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2023-09-20 22:59:20 +0300

    throw error if loading ink file is failed

--- src/interfaces/loader-fetch.js
+++ src/interfaces/loader-fetch.js
@@ -1,6 +1,9 @@
 async function load(path) {
   const response = await fetch(path);
-  return response.text();
+  if (response.ok) {
+    return response.text();
+  }
+  throw new Error(`Failed to load ink file: ${path}`);
 }
 
 export default {
