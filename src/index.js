ddd13669ec0c468b2374e8862f7feb1dc2cad6ec
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2024-05-22 00:23:59 +0300

    v1.2 (#1)
    * add jszip loader
    
    * update sound interface
    
    * update @atrament/core
    
    * separate entry for jszip loader
    
    * use fflate as default loader
    
    * update umd build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import atrament from '@atrament/core';
 
-import loader from './interfaces/loader-web';
+import loader from './interfaces/loader-web-fflate';
 import persistent from './interfaces/persistent-localforage';
 import sound from './interfaces/sound-howler';
 import state from './interfaces/state-nanostores';
