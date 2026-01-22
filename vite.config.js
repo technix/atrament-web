a16338294377c287ba1b53fbbcb2648a67c71eb3
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-01-22 23:19:57 +0200

    reorganize source

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'; /* eslint-disable-line */
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js')
+    },
+    rollupOptions: {
+      output: [{
+        entryFileNames: '[name].cjs',
+        format: 'cjs',
+        dir: 'dist'
+      }, {
+        entryFileNames: '[name].mjs',
+        format: 'es',
+        dir: 'dist'
+      }, {
+        entryFileNames: 'atrament.js',
+        name: 'atrament',
+        format: 'umd',
+        dir: 'umd'
+      }]
+    }
+  }
+});
