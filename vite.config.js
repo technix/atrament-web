7b124ae8a3b0027070d02e7290f78aabef8ed142
Serhii Mozhaiskyi <sergei.mozhaisky@gmail.com>
2026-01-29 15:13:50 +0200

    add bundle size control

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'; /* eslint-disable-line */
+import bundlesize from 'vite-plugin-bundlesize';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [
+    bundlesize({
+      limits: [
+        { name: 'atrament.js', limit: '30 kB', mode: 'gzip' },
+        { name: 'index.cjs', limit: '30 kB', mode: 'gzip' },
+        { name: 'index.mjs', limit: '30 kB', mode: 'gzip' }
+      ]
+    })
+  ],
   build: {
+    sourcemap: 'hidden',
     lib: {
       entry: resolve(__dirname, 'src/index.js')
     },
